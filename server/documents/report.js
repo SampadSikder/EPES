471253616cd62681426074408bcc85b2a28e70f6
SampadSikder <sampadsikder84@gmail.com>
2023-05-22 18:41:04 +0600

    Notifications and reports

--- server/documents/report.js
+++ server/documents/report.js
@@ -1,10 +1,11 @@
 const PDFDocument = require('pdfkit');
 const { Attendance } = require("../models");
 const { Assignments } = require("../models");
+const { Workers } = require("../models");
 const express = require('express');
 const router = express.Router();
 
-const generatePDF = () => {
+const generateattendancePDF = () => {
     return new Promise(async (resolve, reject) => {
         const today = new Date();
 
@@ -16,12 +17,9 @@ const generatePDF = () => {
         // Create new PDF document
         const doc = new PDFDocument();
 
-        // Pipe PDF document to a writable stream and attach it to response
-
         // Set document metadata
         doc.info.Title = 'Attendance and Assignments';
 
-        // Add attendance table to PDF
         doc.fontSize(18).text('Attendance Records for ' + today.toLocaleDateString(), { align: 'center' });
         doc.moveDown();
         doc.fontSize(12);
@@ -45,8 +43,6 @@ const generatePDF = () => {
             doc.font('Courier').text(`${record.assignedWorkplace}                  ${record.workplaceType}`);
         }
 
-
-        // Finalize and end the document
         doc.end();
 
         const buffers = [];
@@ -56,8 +52,55 @@ const generatePDF = () => {
     });
 };
 
+const generateKPIPDF = () => {
+    return new Promise(async (resolve, reject) => {
+        const today = new Date();
+        const currentMonth = today.getMonth();
+        const currentYear = today.getFullYear();
+
+        //if (today.getDate() === 1) {
+        const previousMonth = currentMonth === 0 ? 11 : currentMonth - 1;
+        const previousYear = currentMonth === 0 ? currentYear - 1 : currentYear;
+
+        const workerList = await Workers.findAll();
+
+        // Create new PDF document
+        const doc = new PDFDocument();
+
+        // Pipe PDF document to a writable stream and attach it to response
+
+        // Set document metadata
+        doc.info.Title = 'KPI Report of workers';
+
+        doc.fontSize(18).text('Employee Details for ' + previousMonth + '/' + previousYear, { align: 'center' });
+
+        doc.moveDown();
+        doc.fontSize(12);
+        doc.font('Courier').text('Employee Name     Employee ID     Specialization     KPI');
+
+        for (const worker of workerList) {
+            const { workerID, workerName, specialization, kpi } = worker;
+            const kpiPrint = kpi === null ? 0 : kpi.toPrecision(2);
+            doc.font('Courier').text(`${workerName.substr(0, 8)}              ${workerID}           ${specialization}        ${kpiPrint}`);
+        }
+
+        // Finalize and end the document
+        doc.end();
+
+        const buffers = [];
+        doc.on('data', buffer => buffers.push(buffer));
+        doc.on('end', () => resolve(Buffer.concat(buffers)));
+        doc.on('error', reject);
+        //} else {
+        // resolve();//not end of month
+        //  }
+
+    });
+}
+
+
 router.get('/manpower-report', async (req, res) => {
-    await generatePDF(req, res)
+    await generateattendancePDF(req, res)
         .then(pdf => {
             res.setHeader('Content-Type', 'application/pdf');
             res.setHeader('Content-Disposition', 'attachment; filename=attendance_and_assignments.pdf');
@@ -68,5 +111,19 @@ router.get('/manpower-report', async (req, res) => {
             res.status(500).send('PDF generation failed');
         });
 });
+
+
+router.get('/kpi-report', async (req, res) => {
+    await generateKPIPDF(req, res)
+        .then(pdf => {
+            res.setHeader('Content-Type', 'application/pdf');
+            res.setHeader('Content-Disposition', 'attachment; filename=attendance_and_assignments.pdf');
+            res.send(pdf);
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).send('PDF generation failed');
+        });
+})
 
 module.exports = router;
--- server/routes/Notifications.js
+++ server/routes/Notifications.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const router = express.Router();
-
+const { Op } = require('sequelize');
 const { Notifications } = require("../models");
 
 router.get('/', async (req, res) => {
-    const notification = await Notifications.findAll({
-        order: [['createdAt', 'DESC']],
-    });
-    res.json(notification);
+    const tenMinutesAgo = new Date(new Date() - 10 * 60 * 1000);
+    await Notifications.findAll({
+        where: {
+            [Op.gt]: tenMinutesAgo,
+            [Op.lt]: new Date()
+        },
+    }).then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    })
+
+
 })
 
 module.exports = router;
